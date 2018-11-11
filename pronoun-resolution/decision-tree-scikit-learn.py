496402a321ece0884bd9a2e74a5583c13ca1fa1e
Enrico Teterra <enrico.teterra@signavio.com>
2018-11-11 21:54:07 +0100

    started parsing babI data

---
+++ pronoun-resolution/decision-tree-scikit-learn.py
@@ -0,0 +1,38 @@
+from collections import defaultdict
+import regex
+import numpy as np
+import pandas as pd
+
+# read the babI task data into arrays. Example task data:
+
+# facts[10]                 {
+#                               '1': 'Mary went to the bedroom.', 
+#                               '2': 'John journeyed to the bathroom.'
+#                           } 
+# questions[10]             Where is John?  
+# answers[10]               bathroom 
+# supporting_facts[10]      2
+
+file = pd.read_csv('data/babI/tasks_1-20_v1-2/en/qa1_single-supporting-fact_train.txt', header=None)
+
+task_count          = 0
+facts               = defaultdict(dict)
+questions           = defaultdict(str)
+answers             = defaultdict(str)
+supporting_facts    = defaultdict(str)
+
+for key, row in file.iterrows():
+    line = row.values[0]
+    match = regex.search(r'(?:\d+)\s(?:(?P<question>.*)\t(?P<answer>.*)\t(?P<support>.*))|(?:(?P<fact_num>\d+)\s(?P<fact_text>.*))', line)
+    
+    if match['fact_text'] is not None:
+        num, text = match['fact_num'], match['fact_text']
+        facts[task_count][num] = text
+
+    else:
+        questions[task_count] = match['question']
+        answers[task_count] = match['answer']
+        supporting_facts[task_count] = match['support']
+        task_count += 1
+
+print task_count, facts[10], questions[10], answers[10], supporting_facts[10]
--- readme.md
+++ readme.md
@@ -1,3 +1,42 @@
 setup & install venv:
 - virtualvenv venv
 - source venv/bin/activate
+
+
+Question / Answer Datasets:
+- squad2.0 from stanford https://rajpurkar.github.io/SQuAD-explorer/
+- babi from facebook https://research.fb.com/downloads/babi/
+- Allen AI Science 
+- Quiz Bowl
+- CNN / Daily Mail 
+- MCTest
+- TriviaQA
+- SimpleQuestions
+- WikiQA
+
+Dialogue Datasets:
+- OpenSubtitles dataset 18 
+- Movie Dialog dataset 19
+- Cornell Movie DialoguesCorpus
+
+Semantic Role Labeling Datasets:
+- Proposition Bank
+
+Pronoun Resolution Datasets:
+- OntoNotes 5.0
+- TRAINS93
+
+source: https://arxiv.org/pdf/1805.09461.pdf
+
+how to represent words:
+- 1 hot vectors
+- word embedding
+- ..?
+
+how to encode text:
+- remember the previous and the next words in the sequence
+- lstm? gru?
+
+attention mechanism:
+- which words to focus on
+- memory networks
