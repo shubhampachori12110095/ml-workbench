4abcb04fc27b92eb25c68dcf5c16d4f88a0035aa
Enrico Teterra <enrico.teterra@signavio.com>
2018-12-02 20:37:55 +0100

    reorganized

---
+++ facebook-babI-tasks/vectorizers/ColumnBowVectorizer.py
@@ -0,0 +1,24 @@
+class MultiColumnBagOfWordsVectorizer(object):
+    def __init__(self):
+        pass
+
+    def fit(self, X, y):
+        """X is a dataframe consisting of multiple text columns"""
+        
+        # build vocabulary dictionary
+        y
+        return self
+
+    def transform(self, X):
+        
+        # for each column
+      
+        # tokenise
+        
+        # create n-gram bag of word sparse matrix  
+                
+        # convert bag of words to array
+        
+        # attach to feature list
+        
+        return X # the feature array
---
+++ facebook-babI-tasks/vectorizers/TestColumnBowVectorizer.py
@@ -0,0 +1,14 @@
+import unittest
+
+class TestColumnBowVectorizer(unittest.TestCase):
+
+    def test_vectorizes_dataframe(self):
+        pass
+
+        # given multi column dataframe
+
+        # outputs list of features that correspond to those dataframes
+
+
+if __name__ == '__main__':
+    unittest.main()
