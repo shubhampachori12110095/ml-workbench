f84586a48fbc53cc383870b3b4704c55a7f4cad0
Enrico Teterra <enrico.teterra@signavio.com>
2018-12-12 17:50:06 +0100

    completed refactor

--- src/vectorizers/ColumnBowVectorizer.py
+++ src/vectorizers/ColumnBowVectorizer.py
@@ -4,6 +4,7 @@ from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
 from sklearn.base import TransformerMixin
 from toolz.functoolz import pipe, partial
+from toolz.dicttoolz import merge
 from operator import concat
 
 class ColumnBowVectorizer(TransformerMixin):
@@ -20,11 +21,18 @@ class ColumnBowVectorizer(TransformerMixin):
     def _apply_stopwords_filter(self, tokens):
         return [token for token in tokens if token not in self._stopwords]
 
-    def apply_stopwords_filter_to_tokenlist(self, tokenlist):
+    def _apply_stopwords_filter_to_tokenlist(self, tokenlist):
         return [self._apply_stopwords_filter(tokens) for tokens in tokenlist]
 
+    def _preprocess_row(self, values):
+        return pipe(
+            values,
+            self._tokenize_row_value,
+            self._apply_stopwords_filter_to_tokenlist
+        )
+
     def _get_ngrams(self, tokens, n):
-        return [''.join(tokens[0:i]) for i in n if i <= len(tokens)] 
+        return [''.join(tokens[0:i]) for i in n if i <= len(tokens)]
 
     def _build_vocabulary(self, tokenlist, ngrams):
         return reduce(concat, 
@@ -34,80 +42,39 @@ class ColumnBowVectorizer(TransformerMixin):
         return { word:index for index, word in enumerate(vocabulary) }
 
     def fit(self, df, *_):
+        """
+        build a dictionary from the given phrases that will be used to 
+        transform phrases into bag of word arrays representing that phrase.
+        """
         tokenlist = pipe(
             df,
             self._get_df_column_row_values,
-            self._tokenize_row_value,
-            self.apply_stopwords_filter_to_tokenlist
+            self._preprocess_row
         )
-
         self._vocabulary = self._build_vocabulary(tokenlist, ngrams=[1,2,3])
         self._dictionary = self._build_dictionary(self._vocabulary)
-
         return self
+        
+    def _get_ngram_dictionary_index(self, ngram):
+        dict_key = self._dictionary[ngram]
+        if dict_key is not -1:
+            return dict_key
+
+    def _create_row_vector(self, tokenlist, ngrams):
+        row_ngrams = [self._get_ngrams(tokenlist, ngrams)]
+        row_indices = [self._get_ngram_dictionary_index(ngram) for row in row_ngrams for ngram in row]
+        return merge(
+            dict.fromkeys(range(0, len(self._vocabulary)), 0),
+            dict.fromkeys(row_indices, 1),
+        )
+
+    def _vectorize_feature(self, tokenlist, ngrams):
+        return [self._create_row_vector(tokens, ngrams) for tokens in tokenlist]
 
     def transform(self, df):
         """
-        for each tokenised sentence, convert that into arrays 
-        of which words occurred, where the order of the occurrence
-        matches that of the vocabulary.
+        transform given phrases into bag of word vectors.
         """
-        column_vectors = []
-
-        for column in df:
-            rows = df[column].values
-            column_vector = []
-
-            for row in rows:
-                tokens = word_tokenize(row)
-                row_vector = dict.fromkeys(
-                    range(0, len(self._vocabulary)), 0)
-
-                tokensFiltered = []
-                for token in tokens:
-                    if token not in self._stopwords:
-                        tokensFiltered.append(token)
-
-                for index, token in enumerate(tokensFiltered):
-                    try: 
-                        # 1-grams
-                        dict_key = self._dictionary[token]
-                        if dict_key is not -1:
-                            row_vector[dict_key] = 1
-
-                        # # 2-grams
-                        if (index > 0 and index < len(tokensFiltered)):
-                            ngram = tokensFiltered[index-1] + tokensFiltered[index]
-                            dict_key = self._dictionary[ngram]
-                            if dict_key is not -1:
-                                row_vector[dict_key] = 1
-
-                        # 3-grams
-                        if (index > 1 and index < len(tokensFiltered)):
-                            ngram = tokensFiltered[index-2] + tokensFiltered[index-1] + tokensFiltered[index]
-                            dict_key = self._dictionary[ngram]
-                            if dict_key is not -1:
-                                row_vector[dict_key] = 1
-
-                    except: 
-                        pass
-                    
-                column_vector.append(
-                    list(row_vector.values())
-                )
-                
-            column_vectors.append(column_vector)
-
-        # convert to feature representation
-        num_data_rows = len(column_vectors[0])
-        feature_vectors = []
-
-        for index in range(0, num_data_rows):
-            feature_vector = []
-
-            for column in column_vectors:
-                feature_vector += column[index]
-
-            feature_vectors.append(feature_vector)
-
-        return np.stack(feature_vectors)
+        tokenized_rows = [self._preprocess_row(value) for value in df.values],
+        return [self._vectorize_feature(feature, [1,2,3]) 
+            for row in tokenized_rows for feature in row]
--- test/vectorizers/test_ColumnBowVectorizer.py
+++ test/vectorizers/test_ColumnBowVectorizer.py
@@ -7,7 +7,7 @@ from src.vectorizers.ColumnBowVectorizer import ColumnBowVectorizer
 class TestColumnBowVectorizer(snapshottest.TestCase):
 
     def setUp(self):
-        self.df = pd.read_json('{"fact1":{"0":"John travelled to the hallway.","1":"Daniel went back to the bathroom.","2":"John went to the hallway.","3":"Sandra travelled to the hallway.","4":"Sandra went back to the bathroom."},"fact2":{"0":"Mary journeyed to the bathroom.","1":"John moved to the bedroom.","2":"Sandra journeyed to the kitchen.","3":"John went to the garden.","4":"Sandra moved to the kitchen."},"question":{"0":"Where is John? ","1":"Where is Mary? ","2":"Where is Sandra? ","3":"Where is Sandra? ","4":"Where is Sandra? "},"answer":{"0":"hallway","1":"bathroom","2":"kitchen","3":"hallway","4":"kitchen"}}')
+        self.df = pd.read_json('{"fact1":{"0":"John travelled to the hallway.","1":"Daniel went back to the bathroom.","2":"John went to the hallway.","3":"Sandra travelled to the hallway.","4":"Sandra went back to the bathroom."},"fact2":{"0":"Mary journeyed to the bathroom.","1":"John moved to the bedroom.","2":"Sandra journeyed to the kitchen.","3":"John went to the garden.","4":"Sandra moved to the kitchen."},"question":{"0":"Where is John? ","1":"Where is Mary? ","2":"Where is Sandra? ","3":"Where is Sandra? ","4":"Where is Sandra? "}}')
 
     def test_vectorizes_dataframe(self):
         vectorizer = ColumnBowVectorizer()
@@ -17,7 +17,7 @@ class TestColumnBowVectorizer(snapshottest.TestCase):
         self.assertMatchSnapshot(json.dumps(vectorizer._dictionary))
 
         feature_vectors = vectorizer.transform(self.df)
-        self.assertMatchSnapshot(json.dumps(feature_vectors.tolist()))
+        self.assertMatchSnapshot(json.dumps(feature_vectors))
 
 if __name__ == '__main__':
     unittest.main()
